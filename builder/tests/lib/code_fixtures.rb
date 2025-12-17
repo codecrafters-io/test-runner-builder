7151610b059f26157cd2ee9be76450f026c498e7
Paul Kuruvilla <rohitpaulk@gmail.com>
2025-12-17 20:06:24 +0530

    Update code fixtures

--- builder/tests/lib/code_fixtures.rb
+++ builder/tests/lib/code_fixtures.rb
@@ -3,22 +3,22 @@ module CodeFixtures
 
   DB = {
     "redis-ruby" => {
-      "dockerfile_contents" => File.read(File.join(repository_root, "tests/fixtures/dockerfiles/redis-ruby-3.3.Dockerfile")),
-      "buildpack_slug" => "ruby-3.3",
+      "dockerfile_contents" => File.read(File.join(repository_root, "tests/fixtures/dockerfiles/redis-ruby-3.4.Dockerfile")),
+      "buildpack_slug" => "ruby-3.4",
       "code_dir" => File.join(repository_root, "tests/fixtures/redis-ruby-pass-stage-1"),
       "course_slug" => "redis",
       "language_slug" => "ruby"
     },
     "redis-rust" => {
-      "dockerfile_contents" => File.read(File.join(repository_root, "tests/fixtures/dockerfiles/redis-rust-1.88.Dockerfile")),
-      "buildpack_slug" => "rust-1.88",
+      "dockerfile_contents" => File.read(File.join(repository_root, "tests/fixtures/dockerfiles/redis-rust-1.91.Dockerfile")),
+      "buildpack_slug" => "rust-1.91",
       "code_dir" => File.join(repository_root, "tests/fixtures/redis-rust-pass-stage-1"),
       "course_slug" => "redis",
       "language_slug" => "rust"
     },
     "redis-go" => {
-      "dockerfile_contents" => File.read(File.join(repository_root, "tests/fixtures/dockerfiles/redis-go-1.24.Dockerfile")),
-      "buildpack_slug" => "go-1.24",
+      "dockerfile_contents" => File.read(File.join(repository_root, "tests/fixtures/dockerfiles/redis-go-1.25.Dockerfile")),
+      "buildpack_slug" => "go-1.25",
       "code_dir" => File.join(repository_root, "tests/fixtures/redis-go-pass-stage-1"),
       "course_slug" => "redis",
       "language_slug" => "go"
